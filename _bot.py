edf0bdfdcd09be21c0c2c4ad7b3378b96bc05019
DickinsonBP <77103451+DickinsonBP@users.noreply.github.com>
2023-09-15 18:46:10 +0000

    new role done

--- _bot.py
+++ _bot.py
@@ -1,3 +1,5 @@
+import os
+
 import discord
 from discord.ext import commands
 
@@ -6,44 +8,20 @@ from threading import Thread
 
 from _support_functions import *
 
+token = os.environ['TOKEN']
 
-class EPBot(discord.Client):
-  __commands = []
-  app = Flask('')
+app = Flask('')
 
-  @app.route('/')
-  def home():
-    return "Hello, The bot is running!"
 
-  def runServer(self):
-    self.app.run(host='0.0.0.0', port=8181)
+@app.route('/')
+def home():
+  return "Hello, The bot is running!"
 
-  def keep_alive(self):
-    t = Thread(target=self.runServer)
-    t.start()
 
-  async def on_ready(self):
-    print("Bot acitvated! {0}".format(self.user))
+def runServer():
+  app.run(host='0.0.0.0', port=8181)
 
-  async def on_message(self, message):
-    # we do not want the bot to reply to itself
-    if (message.author.id == self.user.id):
-      return
 
-    for command in self.__commands:
-      if (message.content == command[0]):
-        print("Channel: {0.channel} | User {0.author} : {0.content}".format(
-            message))
-        await message.channel.send(command[1])
-
-  async def on_member_join(self, member):
-    await member.send("Hola!!")
-
-  #basic !command
-  def setCommand(self, command, text):
-    new_command = (command, text)
-    self.__commands.append(new_command)
-
-  def run(self, token):
-    self.keep_alive()
-    return super().run(token)
+def keep_alive():
+  t = Thread(target=runServer)
+  t.start()
--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 import os
 import discord
-from _bot import EPBot
+from discord.ext import commands
+from _bot import keep_alive
 
-intents = discord.Intents.default()
+TOKEN = os.environ['TOKEN']
+
+intents = discord.Intents.all()
 intents.message_content = True
 
-bot = EPBot(intents=intents)
-bot.setCommand("!hello", "Hello!")
-bot.setCommand("!penis", "Esto es un penis jeje :P")
-bot.setCommand("!malo", "no seas malo :P")
-bot.run(os.environ['TOKEN'])
+keep_alive()
+#bot.setCommand("!hello", "Hello!")
+#bot.setCommand("!penis", "Esto es un penis jeje :P")
+#bot.setCommand("!malo", "no seas malo :P")
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+
+@bot.event
+async def on_ready():
+  print("Bot acitvated!")
+
+
+@bot.event
+async def on_member_join(member):
+  role = discord.utils.get(member.guild.roles, name="ExampleRol")
+  await member.add_roles(role)
+
+
+bot.run(TOKEN)
