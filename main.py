63e746699321b18c77fd32b8f44f606a4cd5a55f
AlexMoriana <115938264+AlexMoriana@users.noreply.github.com>
2023-09-15 18:15:32 +0000

    prueba alex

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ intents.message_content = True
 
 bot = EPBot(intents=intents)
 bot.setCommand("!hello", "Hello!")
-bot.setCommand("!penis","Esto es un penis jeje :P")
-
+bot.setCommand("!penis", "Esto es un penis jeje :P")
+bot.setCommand("!malo", "no seas malo :P")
 bot.run(os.environ['TOKEN'])
