29b4969bb13f50a7a64f137c675e1c808824e1aa
DickinsonBedoya <15653913-DickinsonBedoya@users.noreply.replit.com>
2023-09-15 16:50:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+import discord
+
+my_secret = os.environ['TOKEN']
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        print('------')
+
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswith('!hello'):
+            await message.reply('Hello!', mention_author=True)
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run(my_secret)
