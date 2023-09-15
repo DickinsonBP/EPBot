955a8d3de4d22e7384e34ab3e2d5071d2f031d29
DickinsonBP <77103451+DickinsonBP@users.noreply.github.com>
2023-09-15 19:01:43 +0000

    added new command

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ from _bot import keep_alive
 
 TOKEN = os.environ['TOKEN']
 
+#command dictionary
+__commands = {
+    "!hola":
+    "Hola!!!",
+    "!twitch":
+    """Estos son los canales de twitch de mis amigos!
+      - Alis: https://www.twitch.tv/alis_trh
+    """
+}
+
 intents = discord.Intents.all()
 intents.message_content = True
 
@@ -25,6 +35,19 @@ async def on_ready():
 async def on_member_join(member):
   role = discord.utils.get(member.guild.roles, name="ExampleRol")
   await member.add_roles(role)
+
+
+@bot.event
+async def on_message(message):
+  # we do not want the bot to reply to itself
+  if message.author == bot.user:
+    return
+
+  for command, text in __commands.items():
+    if (message.content == command):
+      print("Channel: {0.channel} | User {0.author} : {0.content}".format(
+          message))
+      await message.channel.send(text)
 
 
 bot.run(TOKEN)
