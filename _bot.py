c215cecf3e4e7f7b2fea7255051ab835af5c80c9
DickinsonBP <77103451+DickinsonBP@users.noreply.github.com>
2023-09-15 17:39:18 +0000

    new member join action

--- _bot.py
+++ _bot.py
@@ -2,29 +2,47 @@ import discord
 from flask import Flask
 from threading import Thread
 
+from _support_functions import *
+
+
 class EPBot(discord.Client):
-    __commands = []
-    app = Flask('')
+  __commands = []
+  app = Flask('')
 
-    @app.route('/')
-    def home():
-        return "Hello, The bot is running!"
-    def runServer(self):
-        self.app.run(host='0.0.0.0', port=8181)
-    def keep_alive(self):
-        t = Thread(target=self.runServer)
-        t.start()
-    async def on_ready(self):
-        print("Bot acitvated! {0}".format(self.user))
+  @app.route('/')
+  def home():
+    return "Hello, The bot is running!"
 
-    async def on_message(self, message):
-        for command in self.__commands:
-            if(message.content == command[0]):
-                print("Channel: {0.channel} | User {0.author} : {0.content}".format(message))
-                await message.channel.send(command[1])
-    def setCommand(self, command, text):
-        new_command = (command, text)
-        self.__commands.append(new_command)
-    def run(self, token):        
-        self.keep_alive()
-        return super().run(token)
+  def runServer(self):
+    self.app.run(host='0.0.0.0', port=8181)
+
+  def keep_alive(self):
+    t = Thread(target=self.runServer)
+    t.start()
+
+  async def on_ready(self):
+    print("Bot acitvated! {0}".format(self.user))
+
+  async def on_message(self, message):
+    # we do not want the bot to reply to itself
+    if (message.author.id == self.user.id):
+      return
+
+    for command in self.__commands:
+      if (message.content == command[0]):
+        print("Channel: {0.channel} | User {0.author} : {0.content}".format(
+            message))
+        await message.channel.send(command[1])
+
+  async def on_member_join(self, member):
+    role = discord.utils.get(member.guild.roles, name='NOOB')
+    await member.add_roles(role)
+
+  #basic !command
+  def setCommand(self, command, text):
+    new_command = (command, text)
+    self.__commands.append(new_command)
+
+  def run(self, token):
+    self.keep_alive()
+    return super().run(token)
---
+++ _support_functions.py
@@ -0,0 +1,6 @@
+#script to define all the functions
+import discord
+from threading import Thread
+
+def basic_commands(client, commands, message):
+  pass
