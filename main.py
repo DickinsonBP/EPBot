fca95dfcdeeb5cc29e32920b98f4a46f6e4c83f8
DickinsonBP <77103451+DickinsonBP@users.noreply.github.com>
2023-09-16 13:08:55 +0000

    updated when a new user joins a server sends a message to a specific channel

--- main.py
+++ main.py
@@ -23,20 +23,29 @@ async def on_ready():
 
 @bot.event
 async def on_member_join(member):
+  channel = bot.get_channel(1152589104140259448)
   role = discord.utils.get(member.guild.roles, name="ExampleRol")
   await member.add_roles(role)
+  await channel.send(
+      "Un aplauso, ha entrado otro retrasad@ a **%s** y se llama....: **%s**" %
+      (member.guild.name, member.name))
 
 
-@bot.command(pass_context=True)
+@bot.command()
 async def hola(ctx):
   await ctx.send("Holaaa!!! Soy el fokin EPBot")
 
 
-@bot.command(pass_context=True)
+@bot.command()
 async def twitch(ctx):
   await ctx.send("""Estos son los canales de twitch de mis amigos!
       - Alis: https://www.twitch.tv/alis_trh
     """)
+
+
+@bot.command()
+async def gpt(ctx, content='hola'):
+  await ctx.send("Has enviado el siguiente mensaje: " + str(content))
 
 
 bot.run(TOKEN)
